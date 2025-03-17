47cab647c72503d21c3087d9f2718b47fd5a129d
mayur <motingemayur@gmail.com>
2025-03-17 08:49:49 +0530

    updated updated HomeFragment.java , Urls.java, activity_home.xml, activity_profile.xml, fragment_home.xml, fragment_people.xml, and themes.xml

--- app/src/main/java/com/mayurmotinge/abhyudaya/HomeFragment.java
+++ app/src/main/java/com/mayurmotinge/abhyudaya/HomeFragment.java
@@ -11,6 +11,7 @@ import androidx.core.app.AppLocalesStorageHelper;
 import androidx.fragment.app.Fragment;
 
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -21,15 +22,32 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.mayurmotinge.abhyudaya.supportclasses.Urls;
+
+import org.json.JSONObject;
 
 public class HomeFragment extends Fragment {
 
     CardView cvNotice, cvEvent, cvLiveChat;
 
+    TextView tvNoticeCardTitle, tvNoticeCardDescription, tvNoticeCardSentAt,
+            tvEventCardTitle, tvEventCardDescription, tvEventCardSentAt,
+             tvRecentChat, tvChatSentBy, tvError;
+
     ImageView ivNoticeList, ivEventList;
 
-    LinearLayout llEmergency, llAddFeedback, llMaintenance;
+    LinearLayout llMaintenanceAddFeedbackEmergency;
+
+    ProgressBar progressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,12 +59,29 @@ public class HomeFragment extends Fragment {
         cvEvent = view.findViewById(R.id.cvEventCard);
         cvLiveChat = view.findViewById(R.id.cvLiveChatCard);
 
+        tvNoticeCardTitle = view.findViewById(R.id.tvNoticeCardTitle);
+        tvNoticeCardDescription = view.findViewById(R.id.tvNoticeCardDescription);
+        tvNoticeCardSentAt = view.findViewById(R.id.tvNoticeCardSentAt);
+
+        tvEventCardTitle = view.findViewById(R.id.tvEventCardTitle);
+        tvEventCardDescription = view.findViewById(R.id.tvEventCardDescription);
+        tvEventCardSentAt = view.findViewById(R.id.tvEventCardSentAt);
+
+        tvRecentChat = view.findViewById(R.id.tvRecentChat);
+        tvChatSentBy = view.findViewById(R.id.tvChatSentBy);
+
+        tvError = view.findViewById(R.id.tvError);
+
         ivNoticeList = view.findViewById(R.id.ivNoticeList);
         ivEventList = view.findViewById(R.id.ivEventList);
 
-        llEmergency = view.findViewById(R.id.llEmergency);
-        llAddFeedback = view.findViewById(R.id.llAddFeedback);
-        llMaintenance = view.findViewById(R.id.llMaintenance);
+        llMaintenanceAddFeedbackEmergency = view.findViewById(R.id.llMaintenanceAddFeedbackEmergency);
+
+        progressBar = view.findViewById(R.id.homeProgressBar);
+
+        fetchHomeData();
+
+
 
         cvNotice.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,19 +126,106 @@ public class HomeFragment extends Fragment {
 
         return view;
     }
-    private View setMaintenanceCard(View view) {
 
-        return view;
+    private void fetchHomeData() {
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+
+        progressBar.setVisibility(View.VISIBLE);
+        cvNotice.setVisibility(View.GONE);
+        cvEvent.setVisibility(View.GONE);
+        llMaintenanceAddFeedbackEmergency.setVisibility(View.GONE);
+        cvLiveChat.setVisibility(View.GONE);
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        StringRequest request = new StringRequest(Request.Method.POST, Urls.URL_HOME_API,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressBar.setVisibility(View.GONE);
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            if (jsonObject.getBoolean("success")){
+                                setNoticeCard(jsonObject);
+                                setEventCard(jsonObject);
+                                setLiveChatCard(jsonObject);
+
+                                cvNotice.setVisibility(View.VISIBLE);
+                                cvEvent.setVisibility(View.VISIBLE);
+                                llMaintenanceAddFeedbackEmergency.setVisibility(View.VISIBLE);
+                                cvLiveChat.setVisibility(View.VISIBLE);
+                                progressBar.setVisibility(View.GONE);
+
+                            } else {
+                                showError("Failed to Display Data, Please Try Again.");
+                            }
+
+                        } catch (Exception e) {
+                            showError("Error Parsing Response");
+                            Log.e("Home", "Error Parsing Response", e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                showError("Server/Network error! Check your connection.");
+                Log.e("Home", "Error: "+error.getMessage());
+            }
+        });
+
+        queue.add(request);
+
     }
 
-    private View setNoticeCard(View view) {
-
-        return view;
+    private void showError(String msg){
+        cvNotice.setVisibility(View.GONE);
+        cvEvent.setVisibility(View.GONE);
+        llMaintenanceAddFeedbackEmergency.setVisibility(View.GONE);
+        cvLiveChat.setVisibility(View.GONE);
+        tvError.setVisibility(View.VISIBLE);
+        tvError.setText(msg);
     }
 
-    private View setEventCard(View view) {
+    private void setLiveChatCard(JSONObject jsonObject) {
 
-        return view;
+        try {
+            JSONObject liveChat = jsonObject.getJSONObject("liveChat");
+            tvRecentChat.setText(liveChat.getString("recentChat"));
+            tvChatSentBy.setText(liveChat.getString("sentBy"));
+        } catch (Exception e){
+            showError("Failed to Display Data");
+            Log.e("Home", "Error Parsing Response", e);
+        }
+
+    }
+
+    private void setNoticeCard(JSONObject jsonObject) {
+
+        try {
+            JSONObject notice = jsonObject.getJSONObject("notice");
+            tvNoticeCardTitle.setText(notice.getString("title"));
+            tvNoticeCardDescription.setText(notice.getString("description"));
+            tvNoticeCardSentAt.setText(notice.getString("sentAt"));
+        } catch (Exception e){
+            showError("Failed to Display Data");
+            Log.e("Home", "Error Parsing Response", e);
+        }
+
+    }
+
+    private void setEventCard(JSONObject jsonObject) {
+
+        try {
+            JSONObject event = jsonObject.getJSONObject("event");
+            tvEventCardTitle.setText(event.getString("title"));
+            tvEventCardDescription.setText(event.getString("description"));
+            tvEventCardSentAt.setText(event.getString("sentAt"));
+        } catch (Exception e){
+            showError("Failed to Display Data");
+            Log.e("Home", "Error Parsing Response", e);
+        }
+
     }
 
 
--- app/src/main/java/com/mayurmotinge/abhyudaya/supportclasses/Urls.java
+++ app/src/main/java/com/mayurmotinge/abhyudaya/supportclasses/Urls.java
@@ -1,7 +1,18 @@
 package com.mayurmotinge.abhyudaya.supportclasses;
 
 public class Urls {
-    public static String server = "http:192.168.56.1:80/";
+    public static final String URL_SERVER = "http:192.168.56.1:80/";
+    public static final String URL_HOME_API = URL_SERVER + "home";
+    public static final String URL_NOTICE_API = URL_SERVER + "notice";
+    public static final String URL_EVENT_API = URL_SERVER + "event";
+    public static final String URL_FEEDBACK_API = URL_SERVER + "feedback";
+    public static final String URL_CHAT_API = URL_SERVER + "chat";
+    public static final String URL_LOGIN_API = URL_SERVER + "login";
+    public static final String URL_REGISTER_API = URL_SERVER + "register";
+    public static final String URL_LOGOUT_API = URL_SERVER + "logout";
+    public static final String URL_EMERGENCY_API = URL_SERVER + "emergency";
+    public static final String URL_ABOUT_API = URL_SERVER + "about";
+
 
 
 }
